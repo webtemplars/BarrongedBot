0bd70539214c89ac4b4ad735d98428c33cd6cbc8
capuno <facundohuetagoyena1@gmail.com>
2017-08-26 06:08:35 +0200

    Added checkboxes for method and more work on output.php

--- website/output.php
+++ website/output.php
@@ -2,20 +2,40 @@
 
 $script = ''; // The location of the script
 $sid = $GET_['sid'];
+$method = $GET_['method'];
+$passw = $GET_['passw'];
+$hPassword = md5($passw)
+$oPassword = "6b17a6cd9cc29d3f67f8c8edc39fed82"; // palceholder for the moment xd
 
-$exit_status = exec('python3.6 $script $sid');
+#if ($oPassword == $hPassword) {
+	#$exit_status = exec('python3.6 $script $method $sid');
+#}
 
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Barrong Bot</title>
+	<title>Barronged Bot</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 
-	<div id="mainform">
+	<div id="mainform" style="height: 300px; margin-top: -150px">
+
+		<b>
+
+			<?php 
+				if ($hPassword != $oPassword) {
+					echo " [>] Wrong Password kiddo":
+				} else {
+					echo " [>] Sending $method to $sid."
+				}
+			 ?>
+			 
+		</b>
+
 		<?php echo $exit_status; ?>
+
 	</div>
 
 </body>
